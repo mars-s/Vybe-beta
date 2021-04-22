3a4d74ee3dad5e3a91174c59b72aac870e8c8b88
ScriptedRain <71009876+ScriptedRain@users.noreply.github.com>
2021-04-22 16:25:40 +1000

    initialised firebase/firestore :fire:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+import 'firebase/auth'
+
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBb6ImeMH66Evn-E8HEY-UV6ngwZ3lYd0s",
+  authDomain: "react-chat-app-b451d.firebaseapp.com",
+  databaseURL: "https://react-chat-app-b451d-default-rtdb.firebaseio.com",
+  projectId: "react-chat-app-b451d",
+  storageBucket: "react-chat-app-b451d.appspot.com",
+  messagingSenderId: "669564407822",
+  appId: "1:669564407822:web:2fdc4da57bd5916f282c5f",
+  measurementId: "G-ELYX8M7QHP"
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
   );
 }
