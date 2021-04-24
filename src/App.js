b7a498d245270a7a36b4a68c19cdea835eed196b
ScriptedRain <71009876+ScriptedRain@users.noreply.github.com>
2021-04-24 16:41:54 +1000

    Cleaned up code

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ import { useCollectionData } from 'react-firebase-hooks/firestore'
 
 firebase.initializeApp({
   apiKey: "AIzaSyBb6ImeMH66Evn-E8HEY-UV6ngwZ3lYd0s",
-  authDomain: "react-chat-app-b451d.firebaseapp.com",
-  databaseURL: "https://react-chat-app-b451d-default-rtdb.firebaseio.com",
-  projectId: "react-chat-app-b451d",
-  storageBucket: "react-chat-app-b451d.appspot.com",
-  messagingSenderId: "669564407822",
-  appId: "1:669564407822:web:2fdc4da57bd5916f282c5f",
-  measurementId: "G-ELYX8M7QHP"
+    authDomain: "react-chat-app-b451d.firebaseapp.com",
+    databaseURL: "https://react-chat-app-b451d-default-rtdb.firebaseio.com",
+    projectId: "react-chat-app-b451d",
+    storageBucket: "react-chat-app-b451d.appspot.com",
+    messagingSenderId: "669564407822",
+    appId: "1:669564407822:web:2fdc4da57bd5916f282c5f",
+    measurementId: "G-ELYX8M7QHP"
 })
 
 const auth = firebase.auth()
