63dfb2d39d924112529f20ee136995a17f6b6eac
ScriptedRain <71009876+ScriptedRain@users.noreply.github.com>
2021-04-24 17:19:24 +1000

    Added the send message feature
    this takes the text in the message box and puts it in the database, and update the app to show the screen but the messages are not showing up

--- src/components/Channel.jsx
+++ src/components/Channel.jsx
@@ -3,6 +3,9 @@ import firebase from 'firebase/app'
 
 const Channel = ({ user = null, db = null }) => {
   const [messages, setMessages] = useState([])
+  const [newMessage, setNewMessage] = useState('')
+
+  const { uid, displayName, photoURL } = user
 
   useEffect(() => {
     if (db) {
@@ -25,12 +28,39 @@ const Channel = ({ user = null, db = null }) => {
     }
   }, [db])
 
+  const handleOnChange = e => {
+    setNewMessage(e.target.value)
+  }
+
+  const handleOnSubmit = e => {
+    e.preventDefault();
+
+    if (db) {
+      db.collection('messages').add({
+        text: newMessage,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        uid,
+        displayName,
+        photoURL
+      })
+    }
+
+  }
+
   return (
-    <ul>
-      {messages.map(message => (
-        <li key={message.id}>{message.text}</li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {messages.map(message => {
+          <li key={message.id}>{message.text}</li>
+        })} 
+      </ul>
+      <form onSubmit={handleOnSubmit}>
+        <input type="text" value={newMessage} onChange={handleOnChange} placeholder="Type Your message here..." />
+        <button type="submit" disabled={ !newMessage}>
+          Send
+        </button>
+      </form>
+    </>
   )
 }
 
