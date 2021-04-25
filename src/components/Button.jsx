c28e0cdd568e37cc830212f7ac6ff8963eaaca68
ScriptedRain <71009876+ScriptedRain@users.noreply.github.com>
2021-04-25 17:58:38 +1000

    Revert "Changed styles dramatically looks alot better now"
    This reverts commit db40ef09c711f91b9537ccb546587d6dc1eab851.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,7 @@
-import { FcGoogle } from 'react-icons/fc'
-
-const Button = ({ onClick = null, children = null, styles }) => {
+const Button = ({ onClick = null, children = null }) => {
   return (
     <div>
-      <button onClick={onClick} className={styles}>{children}</button>
+      <button onClick={onClick} className={'text-lg drop-shadow-md bg-purple-400 rounded-full py-3 px-6 font-sans font-medium text-lg'}>{children}</button>
     </div>
   )
 }
