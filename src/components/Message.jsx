bed2eaeb676da9a4afeabf23b1f0f8e700d68840
Bakhos Ibrahim-Rachwan <bakhos.rachwan@gmail.com>
2021-04-26 12:36:37 +0300

    Style: format layout

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -8,15 +8,21 @@ const Message = ({
   photoURL = '',
 }) => {
   return (
-    <div>
-      {photoURL ? (
-        <img src={photoURL} alt="avatar" width={45} height={45} />
-      ) : null}
-      {displayName ? <p>{displayName}</p> : null}
-      {createdAt?.seconds ? (
-        <span>{formatRelative(new Date(createdAt.seconds * 1000), new Date())}</span>
-      ): null}
-      <p>{text}</p>
+    <div className={'flex'}>
+      <div className={'inline-block mb-6 h-16 pr-5 leading-10'}>
+        {photoURL ? (
+          <img className={'rounded-full float-left h-full'} src={photoURL} alt="avatar"/>
+        ) : <img className={'rounded-full float-left h-full'} src={'https://ssl.gstatic.com/accounts/ui/avatar_2x.png'} alt="avatar"/> }
+      </div>
+      <div>
+        <div className={'flex-1'}>
+        {displayName ? <span className="ml-3">{displayName}</span> : <span className="ml-3">John Doe</span>}
+        {createdAt?.seconds ? (
+          <span className={'text-gray-500 ml-3 text-sm font-semibold tracking-wide'}>{formatRelative(new Date(createdAt.seconds * 1000), new Date())}</span>
+        ): null}
+        </div>
+      <p className={'ml-3'}>{text}</p>
+      </div>
     </div>
   )
 }
