85e00d802baeffd0027fd99c4ffe5081dbf8f954
Bakhos Ibrahim-Rachwan <bakhos.rachwan@gmail.com>
2021-04-28 13:42:28 +0300

    Config: custom height

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        screens: '80vh'
+    }
+    },
   },
   variants: {
     extend: {},
